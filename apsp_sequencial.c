f96f9c33a19c42da3f931a05f10182d69cb05e79
jhonathan <jhonathanduartebarreto@gmail.com>
2025-11-20 14:49:05 -0400

    lendo o grafo do arquivo

--- apsp_sequencial.c
+++ apsp_sequencial.c
@@ -13,36 +13,88 @@ typedef struct {
 
 // aloca o grafo e retorna o ponteiro para o grafo criado
 Grafo *criar_grafo(int n) {
-  Grafo *g = (Grafo *)malloc(sizeof(Grafo));
-  g->n = n;
+    Grafo *g = (Grafo *)malloc(sizeof(Grafo));
+    g->n = n;
+    // aloca a matriz de adjc
+    g->adj = (int **)malloc(n * sizeof(int *));
+    for (int i = 0; i < n; i++) {
+        g->adj[i] = (int *)malloc(n * sizeof(int));
+    }
 
-  // aloca a matriz de adjc
-  g->adj = (int **)malloc(n * sizeof(int *));
-  for (int i = 0; i < n; i++) {
-    g->adj[i] = (int *)malloc(n * sizeof(int));
-  }
-
-  return g;
+    return g;
 }
 
 void liberar_grafo(Grafo *g) {
-  for (int i = 0; i < g->n; i++) {
-    free(g->adj[i]);
-  }
-  free(g->adj);
-  free(g);
+    for (int i = 0; i < g->n; i++) {
+        free(g->adj[i]);
+    }
+    free(g->adj);
+    free(g);
+}
+
+
+Grafo *ler_grafo(const char *nome_arquivo) {
+    FILE *arquivo = fopen(nome_arquivo, "r");
+
+    int n;
+    fscanf(arquivo, "%d", &n);
+
+    printf("Lendo grafo com %d vertices...\n", n);
+    Grafo *g = criar_grafo(n);
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            char buffer[64]; // buffer para ler a linha
+            fscanf(arquivo, "%s", buffer);
+
+            // verifica se é INF
+            if(buffer[0] == 'I' || buffer[0] == 'i'){
+                g->adj[i][j] = INF;
+            }else{
+                g->adj[i][j] = atoi(buffer);
+            }
+        }
+    }
+
+    fclose(arquivo);
+    printf("Grafo lido com sucesso!\n");
+    return g;
+}
+
+void imprimir_grafo(int **matriz, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (matriz[i][j] == INF) {
+                printf("  INF");
+            } else {
+                printf("%5d", matriz[i][j]);
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
 int main(int argc, char *argv[]) {
-  printf("=== APSP - Versão Sequencial ===\n");
+    printf("=== APSP - Versão Sequencial ===\n\n");
 
-  int n = 4; //
-  Grafo *g = criar_grafo(n);
+    // Verificar argumentos da linha de comando
+    if (argc < 2) {
+        printf("Uso: %s <arquivo_grafo>\n", argv[0]);
+        printf("Exemplo: %s grafo.txt\n", argv[0]);
+        return 1;
+    }
 
-  printf("Grafo com %d vertices criado com sucesso!\n", g->n);
+    // Ler o grafo do arquivo
+    Grafo *g = ler_grafo(argv[1]);
+    if (g == NULL) {
+        return 1;
+    }
 
-  // libera memoria
-  liberar_grafo(g);
+    imprimir_grafo(g->adj, g->n);
 
-  return 0;
+    // libera memoria
+    liberar_grafo(g);
+
+    return 0;
 }
